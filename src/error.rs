cfeaa8e244d0af2b8c4747b5914f8e2bd16d3889
lex <lex@work.cn>
2026-01-10 22:21:26 +0800

    use Enum as Error

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,32 @@
+use std::fmt::{Display, Formatter};
+use derive_more::From;
 
 pub type Result<T> = core::result::Result<T, Error>;
-pub type Error = Box<dyn std::error::Error>;
+#[derive(Debug, From)]
+pub enum Error {
+    #[from]
+    Custom(String),
+
+    #[from]
+    Io(std::io::Error)
+}
+
+impl Error {
+    pub fn custom(msg: impl Display) -> Self {
+        Self::Custom(msg.to_string())
+    }
+}
+
+impl From<&str> for Error {
+    fn from(value: &str) -> Self {
+        Self::Custom(value.to_string())
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {}
